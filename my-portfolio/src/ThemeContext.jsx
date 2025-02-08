a43bd8100d4dd0f16b16b3407a7602aa703d9345
Akshay Kumar S <akshaykumars9108@gmail.com>
2025-02-08 15:42:59 +0530

    index.html page updated

--- my-portfolio/src/ThemeContext.jsx
+++ my-portfolio/src/ThemeContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
